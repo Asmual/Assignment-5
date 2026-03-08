bac917819102f7c9e12d5334548fbe6b4409b740
Asmual <Asmual04@gmail.com>
2026-03-08 16:25:57 +0600

    issues from API & loading spinner

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.getElementById('login-btn').addEventListener('click', function() {
+ /* Login page Scripts */
+
+document.getElementById('login-btn')?.addEventListener('click', function () {
     const username = document.getElementById('username-field').value;
     const password = document.getElementById('password-field').value;
 
@@ -8,3 +10,76 @@ document.getElementById('login-btn').addEventListener('click', function() {
         alert("Invalid username or password. Please try again.");
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+/* Main Page Scripts */
+
+// API url
+const API_URL = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+/*Gets Element*/
+
+const issuesContainer = document.getElementById("issues-container");
+const issuesCount = document.getElementById("issues-count");
+
+const allBtn = document.getElementById("all-btn");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+
+const searchInput = document.getElementById("search-input");
+
+const modal = document.getElementById("issue-modal");
+
+const loading = document.getElementById("loading");
+
+let allIssues = [];
+
+
+/*Loading Spinner*/
+
+function showLoading() {
+  loading.classList.remove("hidden");
+}
+
+function hideLoading() {
+  loading.classList.add("hidden");
+}
+
+
+/*Load All Issues-API*/
+
+async function loadIssues() {
+
+  try {
+
+    showLoading();
+
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    allIssues = data.data;
+
+    displayIssues(allIssues);
+
+  } catch (error) {
+
+    console.log("Error loading issues:", error);
+
+  } finally {
+
+    hideLoading();
+
+  }
+
+}
+
+loadIssues();
